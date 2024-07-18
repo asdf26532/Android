c660c409ea45c3e13c2eb11430f5286db6a3e9ce
SeungHwan Lee <asdf26532@gmail.com>
2024-07-18 17:18:35 +0900

    back

--- Hello/app/src/main/java/practice/org/MenuActivity.java
+++ Hello/app/src/main/java/practice/org/MenuActivity.java
@@ -1,10 +1,7 @@
 package practice.org;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -26,18 +23,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-   public void onButton1Clicked(View v) {
-       Toast.makeText(getApplicationContext(),"버튼 눌러짐", Toast.LENGTH_LONG).show();
+    public void onBackClicked(View v) {
+        finish();
     }
-
-    public void onButton2Clicked(View v) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
-        startActivity(intent);
-    }
-
-    public void onButton3Clicked(View v){
-        Intent intent = new Intent(this, MenuActivity.class);
-        startActivity(intent);
-    }
-
 }
