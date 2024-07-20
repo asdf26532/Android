03291b084e1ef90941da764df11f868ca841de20
SeungHwan Lee <asdf26532@gmail.com>
2024-07-20 19:34:10 +0900

    intent example

---
+++ IntentExample/app/src/main/java/com/intentexample/MainActivity.java
@@ -0,0 +1,47 @@
+package com.intentexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_move;
+    private EditText et_test;
+    private String str;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        et_test = findViewById(R.id.et_test);
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                str = et_test.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("str", str);
+                startActivity(intent);      // 액티비티 이동
+            }
+        });
+
+    }
+}
---
+++ IntentExample/app/src/main/java/com/intentexample/SubActivity.java
@@ -0,0 +1,38 @@
+package com.intentexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SubActivity extends AppCompatActivity {
+
+    private TextView tv_sub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sub);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tv_sub = findViewById(R.id.tv_sub);
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("str");
+
+        tv_sub.setText(str);
+
+
+    }
+}
