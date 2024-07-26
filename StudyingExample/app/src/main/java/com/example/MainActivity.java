e1e2658adcc98e97a30f1ac2f3156fd2a23aa0f7
SeungHwan Lee <asdf26532@gmail.com>
2024-07-26 20:42:06 +0900

    Thread Example

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example;
 
 import android.os.Bundle;
-
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,35 +21,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        btn_start = (Button) findViewById(R.id.btn_start);
-        btn_stop = (Button) findViewById(R.id.btn_stop);
+        btn_start = findViewById(R.id.btn_start);
+        btn_stop = findViewById(R.id.btn_stop);
 
-        // 쓰레드 시작
+        Handler handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                Log.d("HandlerDebug", "Toast message is being shown");
+                Toast.makeText(MainActivity.this, "오류 발생", Toast.LENGTH_LONG).show();
+            }
+        };
+
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 isThread = true;
-                thread = new Thread(){
-                  public void run(){
-                      while (isThread){
-                          try {
-                              sleep(5000);
-                          } catch (InterruptedException e) {
-                              throw new RuntimeException(e);
-                          }
-                          handler.sendEmptyMessage(0);
-                      }
-                  }
+                thread = new Thread() {
+                    public void run() {
+                        while (isThread) {
+                            try {
+                                sleep(5000);
+                                Log.d("ThreadDebug", "Handler message sent");
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            handler.sendEmptyMessage(0);
+                        }
+                    }
                 };
 
                 thread.start();
             }
         });
 
-        // 쓰레드 종료
         btn_stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,12 +64,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            Toast.makeText(getApplicationContext(), "오류 발생", Toast.LENGTH_SHORT).show();
-        }
-    };
-
 }
