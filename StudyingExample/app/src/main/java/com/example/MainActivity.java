9349ba8c1fa066099c1095020b5f6c669fd6187d
SeungHwan Lee <asdf26532@gmail.com>
2024-07-29 23:13:05 +0900

    video recording

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -1,43 +1,52 @@
 package com.example;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Button;
+
+
+import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.gun0912.tedpermission.normal.TedPermission;
 
-    Button btn_start,btn_stop;
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
+    private Camera camera;
+    private MediaRecorder mediaRecorder;
+    private Button btn_record;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private boolean recording = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       btn_start = (Button) findViewById(R.id.btn_start);
-       btn_stop = (Button) findViewById(R.id.btn_stop);
+        TedPermission.with(this)
+                .setp
 
-       btn_start.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               startService(new Intent(getApplicationContext(), MusicService.class));
 
-           }
-       });
+    }
 
-       btn_stop.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               stopService(new Intent(getApplicationContext(), MusicService.class));
-           }
-       });
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
 
     }
 }
