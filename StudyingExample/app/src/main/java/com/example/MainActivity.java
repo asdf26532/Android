52340672ee91fec24688d4ae9fa969672638dc85
SeungHwan Lee <asdf26532@gmail.com>
2024-07-31 17:26:25 +0900

    Spinner Example

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -1,66 +1,39 @@
 package com.example;
 
 
-import android.hardware.Camera;
-import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.Button;
-import android.widget.Toast;
-
-
-import androidx.annotation.NonNull;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.gun0912.tedpermission.PermissionListener;
-import com.gun0912.tedpermission.normal.TedPermission;
 
-import java.util.List;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
-    private Camera camera;
-    private MediaRecorder mediaRecorder;
-    private Button btn_record;
-    private SurfaceView surfaceView;
-    private SurfaceHolder surfaceHolder;
-    private boolean recording = false;
+    private Spinner spinner;
+    private TextView tv_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        spinner = (Spinner) findViewById(R.id.spinner);
+        tv_result = (TextView) findViewById(R.id.tv_result);
 
-    PermissionListener permissionlistener = new PermissionListener() {
-        @Override
-        public void onPermissionGranted() {
-            Toast.makeText(MainActivity.this, "권한이 허용되었습니다", Toast.LENGTH_SHORT).show();
-        }
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                tv_result.setText(parent.getItemAtPosition(position).toString());
+            }
 
-        @Override
-        public void onPermissionDenied(List<String> deniedPermissions) {
-            Toast.makeText(MainActivity.this, "권한이 거부되었습니다\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
-        }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-
-    };
-
-    @Override
-    public void surfaceCreated(@NonNull SurfaceHolder holder) {
-
-    }
-
-    @Override
-    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-
+            }
+        });
     }
 }
