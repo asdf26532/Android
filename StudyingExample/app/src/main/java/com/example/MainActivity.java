08c654fd679401347537dd746963694b18533a1b
SeungHwan Lee <asdf26532@gmail.com>
2024-08-01 18:44:23 +0900

    musicPlayer Example

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -1,39 +1,61 @@
 package com.example;
 
-
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_play, btn_stop;
 
-    private Spinner spinner;
-    private TextView tv_result;
+    MediaPlayer mediaPlayer;
+
+    // 액티비티 종료시 실행
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = (Spinner) findViewById(R.id.spinner);
-        tv_result = (TextView) findViewById(R.id.tv_result);
+        btn_play = (Button) findViewById(R.id.btn_play);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        // 재생 버튼
+        btn_play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                tv_result.setText(parent.getItemAtPosition(position).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+            public void onClick(View v) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.music);
+                mediaPlayer.start();
             }
         });
+
+        // 정지 버튼
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                }
+            }
+        });
+
+
+
+
     }
 }
