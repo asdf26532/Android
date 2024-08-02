1443c3ecc4cf43cb4bf4ff968a1212dd802b5a24
SeungHwan Lee <asdf26532@gmail.com>
2024-08-02 21:30:24 +0900

    backBtn off

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -1,61 +1,39 @@
 package com.example;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.activity.OnBackPressedDispatcher;
+import androidx.activity.OnBackPressedDispatcherOwner;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_play, btn_stop;
-
-    MediaPlayer mediaPlayer;
-
-    // 액티비티 종료시 실행
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(mediaPlayer != null){
-            mediaPlayer.release();
-            mediaPlayer = null;
-        }
-
-
-    }
+    private long backBtnTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_play = (Button) findViewById(R.id.btn_play);
-        btn_stop = (Button) findViewById(R.id.btn_stop);
+    }
 
-        // 재생 버튼
-        btn_play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.music);
-                mediaPlayer.start();
-            }
-        });
+    @Override
+    public void onBackPressed() {
 
-        // 정지 버튼
-        btn_stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mediaPlayer.isPlaying()) {
-                    mediaPlayer.stop();
-                    mediaPlayer.reset();
-                }
-            }
-        });
+        long curTime = System.currentTimeMillis();
+        long gapTime = curTime - backBtnTime;
 
+        if(0 <= gapTime && 2000 >= gapTime){
+            super.onBackPressed();
+        }
 
+        else {
+            backBtnTime = curTime;
+            Toast.makeText(this, "한번 더 누르면 종료됩니다" ,Toast.LENGTH_SHORT ).show();
 
+        }
 
     }
 }
