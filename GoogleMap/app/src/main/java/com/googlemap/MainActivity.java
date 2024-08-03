dc0842f7d6f4c59d3a5a81e5d92a41a4bd262118
SeungHwan Lee <asdf26532@gmail.com>
2024-08-03 21:41:07 +0900

    api test

--- GoogleMap/app/src/main/java/com/googlemap/MainActivity.java
+++ GoogleMap/app/src/main/java/com/googlemap/MainActivity.java
@@ -1,24 +1,49 @@
 package com.googlemap;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        LatLng location = new LatLng(35.17531, 129.1822);  // 아세안 문화원
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("아세안 문화원");
+        markerOptions.snippet("집 앞");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+       // googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+
     }
 }
