c88c1f7318ade32ab7ea34b51da37560f1e8ac04
SeungHwan Lee <asdf26532@gmail.com>
2024-08-04 23:52:48 +0900

    Bottom Navigationbar

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -1,39 +1,88 @@
 package com.example;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.activity.OnBackPressedDispatcher;
 import androidx.activity.OnBackPressedDispatcherOwner;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private long backBtnTime = 0;
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Frag1 frag1;
+    private Frag2 frag2;
+    private Frag3 frag3;
+    private Frag4 frag4;
+    private Frag5 frag5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.s:
+                        setFrag(0);
+                        break;
+                }
+
+                return true;
+            }
+        });
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+        frag4 = new Frag4();
+        frag5 = new Frag5();
+
+        setFrag(0); // 첫프래그먼트 화면
+
     }
 
-    @Override
-    public void onBackPressed() {
+    private void setFrag(int n){
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch(n){
+            case 0:
+                ft.replace(R.id.main_frame, frag1);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, frag2);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, frag3);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame, frag4);
+                ft.commit();
+                break;
+            case 4:
+                ft.replace(R.id.main_frame, frag5);
+                ft.commit();
+                break;
 
-        long curTime = System.currentTimeMillis();
-        long gapTime = curTime - backBtnTime;
-
-        if(0 <= gapTime && 2000 >= gapTime){
-            super.onBackPressed();
         }
-
-        else {
-            backBtnTime = curTime;
-            Toast.makeText(this, "한번 더 누르면 종료됩니다" ,Toast.LENGTH_SHORT ).show();
-
-        }
-
     }
+
+
+
 }
