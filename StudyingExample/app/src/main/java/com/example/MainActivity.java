6c01337e9a184ddbd1a4de2323fa9a14dee360bb
SeungHwan Lee <asdf26532@gmail.com>
2024-08-05 19:05:14 +0900

    new

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -1,29 +1,11 @@
 package com.example;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-import androidx.activity.OnBackPressedDispatcher;
-import androidx.activity.OnBackPressedDispatcherOwner;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private BottomNavigationView bottomNavigationView;
-    private FragmentManager fm;
-    private FragmentTransaction ft;
-    private Frag1 frag1;
-    private Frag2 frag2;
-    private Frag3 frag3;
-    private Frag4 frag4;
-    private Frag5 frag5;
 
 
     @Override
@@ -31,64 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navi);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-                int itemId = menuItem.getItemId();
-                if (itemId == R.id.airplane) {
-                    setFrag(0);
-                } else if (itemId == R.id.man) {
-                    setFrag(1);
-                } else if (itemId == R.id.ring) {
-                    setFrag(2);
-                } else if (itemId == R.id.call) {
-                    setFrag(3);
-                } else if (itemId == R.id.security) {
-                    setFrag(4);
-                }
-                return true;
-            }
-        });
-        frag1 = new Frag1();
-        frag2 = new Frag2();
-        frag3 = new Frag3();
-        frag4 = new Frag4();
-        frag5 = new Frag5();
-
-        setFrag(0); // 첫프래그먼트 화면
 
     }
-
-    private void setFrag(int n){
-        fm = getSupportFragmentManager();
-        ft = fm.beginTransaction();
-        switch(n){
-            case 0:
-                ft.replace(R.id.main_frame, frag1);
-                ft.commit();
-                break;
-            case 1:
-                ft.replace(R.id.main_frame, frag2);
-                ft.commit();
-                break;
-            case 2:
-                ft.replace(R.id.main_frame, frag3);
-                ft.commit();
-                break;
-            case 3:
-                ft.replace(R.id.main_frame, frag4);
-                ft.commit();
-                break;
-            case 4:
-                ft.replace(R.id.main_frame, frag5);
-                ft.commit();
-                break;
-
-        }
-    }
-
-
-
 }
