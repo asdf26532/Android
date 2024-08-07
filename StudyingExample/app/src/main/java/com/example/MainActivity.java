3262f88b375d876bd994dae3a745b55440519209
SeungHwan Lee <asdf26532@gmail.com>
2024-08-07 21:43:51 +0900

    Network state check

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example;
 
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    public static TextView tv_state;
+    private NetworkReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv_state = findViewById(R.id.tv_state);
 
+        IntentFilter filter = new IntentFilter();
+        receiver = new NetworkReceiver();
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        registerReceiver(receiver, filter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 브로드캐스트 리시버 해제
+        unregisterReceiver(receiver);
     }
 }
