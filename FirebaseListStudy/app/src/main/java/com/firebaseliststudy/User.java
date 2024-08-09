88f53aa0d9a214d6e1e621e0375e02c9fe565560
SeungHwan Lee <asdf26532@gmail.com>
2024-08-09 17:35:05 +0900

    list item

---
+++ FirebaseListStudy/app/src/main/java/com/firebaseliststudy/User.java
@@ -0,0 +1,43 @@
+package com.firebaseliststudy;
+
+public class User {
+    private String profile;
+    private String id;
+    private int password;
+    private String UserName;
+
+
+    public User(){}
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+}
