2d77a95cab4afc8dd5446bc69322faa11602ade0
SeungHwan Lee <asdf26532@gmail.com>
2024-08-11 19:53:27 +0900

    basic setting

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -1,19 +1,11 @@
 package com.example;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.MediaController;
-import android.widget.VideoView;
-
-
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private VideoView videoView;
-    private MediaController mediaController;
-    private String videoURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp3";
 
 
     @Override
@@ -21,15 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        videoView = (VideoView) findViewById(R.id.videoView);
-
-        mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        Uri uri = Uri.parse(videoURL);
-        videoView.setMediaController(mediaController);
-        videoView.setVideoURI(uri);
-
-        videoView.start();
 
 
 
