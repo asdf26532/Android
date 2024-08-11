8a3decde407b0982c53a0ad8b3fc847eea127ac3
SeungHwan Lee <asdf26532@gmail.com>
2024-08-11 21:11:30 +0900

    ViewPager

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentPagerAdapter fragmentPagerAdapter;
 
 
 
@@ -13,7 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        fragmentPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
 
     }
