f8edb36ca6e9617417e99a7b6b008227ebbbf9c2
SeungHwan Lee <asdf26532@gmail.com>
2024-08-16 22:02:04 +0900

    fragment example

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -2,7 +2,7 @@ package com.example;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment1 fragment1 = new Fragment1();
+        transaction.replace(R.id.frameLayout, fragment1);
+        transaction.commit();
 
 
     }
