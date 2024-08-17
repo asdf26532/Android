d071cfff150d7a2a2c8f6b25f28ec09aaa14618f
SeungHwan Lee <asdf26532@gmail.com>
2024-08-17 21:55:13 +0900

    cardview

--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -2,7 +2,7 @@ package com.example;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        Fragment1 fragment1 = new Fragment1();
-        transaction.replace(R.id.frameLayout, fragment1);
-        transaction.commit();
 
 
     }
