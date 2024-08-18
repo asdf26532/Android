c74db1875788d725b6ff3a4c7186f4b5a6a350fa
SeungHwan Lee <asdf26532@gmail.com>
2024-08-18 21:15:55 +0900

    view overlap

--- StudyingExample/app/build.gradle.kts
+++ StudyingExample/app/build.gradle.kts
@@ -38,7 +38,6 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.material)
-    implementation("androidx.cardview:cardview:1.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- StudyingExample/app/src/main/java/com/example/MainActivity.java
+++ StudyingExample/app/src/main/java/com/example/MainActivity.java
@@ -15,7 +15,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-
     }
 }
