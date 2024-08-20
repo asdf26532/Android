d24bda5867423ca3b9b2a04a65988638fa8315b3
SeungHwan Lee <asdf26532@gmail.com>
2024-08-20 22:44:25 +0900

    update login

--- KakaoLogin/app/src/main/java/com/kakaologin/MainActivity.java
+++ KakaoLogin/app/src/main/java/com/kakaologin/MainActivity.java
@@ -1,17 +1,56 @@
 package com.kakaologin;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.kakao.sdk.user.UserApi;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.User;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private View loginButton, logoutButton;
+    private TextView nickName;
+    private ImageView profileImage;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginButton = findViewById(R.id.btn_login);
+        logoutButton = findViewById(R.id.btn_logout);
+        nickName = findViewById(R.id.nickname);
+        profileImage = findViewById(R.id.profile);
 
+        updateKakaoLoginUi();
+    }
+
+
+    private void updateKakaoLoginUi() {
+        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+            @Override
+            public Unit invoke(User user, Throwable throwable) {
+                if (user != null) {
+                    loginButton.setVisibility(View.GONE);
+                    logoutButton.setVisibility(View.VISIBLE);
+                } else {
+                    loginButton.setVisibility(View.VISIBLE);
+                    logoutButton.setVisibility(View.GONE);
+                }
+                return null;
+            }
+        });
     }
 
 }
