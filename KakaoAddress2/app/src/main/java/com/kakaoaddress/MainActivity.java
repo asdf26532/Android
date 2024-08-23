c528dbf9ca65a3c1043d5973946d8e3c5c2ff15a
SeungHwan Lee <asdf26532@gmail.com>
2024-08-23 21:05:27 +0900

    search address

--- KakaoAddress2/app/src/main/java/com/kakaoaddress/MainActivity.java
+++ KakaoAddress2/app/src/main/java/com/kakaoaddress/MainActivity.java
@@ -1,6 +1,9 @@
 package com.kakaoaddress;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText mEtAddress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mEtAddress = findViewById(R.id.et_adddress);
+        mEtAddress.setFocusable(false);
+        mEtAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, );
+
+
+            }
         });
+
     }
 }
