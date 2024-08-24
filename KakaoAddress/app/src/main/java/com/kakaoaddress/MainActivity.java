3e434b412be3038095a0beb77ff7f43b85151a24
SeungHwan Lee <asdf26532@gmail.com>
2024-08-24 19:14:02 +0900

    search address API

--- KakaoAddress/app/src/main/java/com/kakaoaddress/MainActivity.java
+++ KakaoAddress/app/src/main/java/com/kakaoaddress/MainActivity.java
@@ -1,11 +1,15 @@
 package com.kakaoaddress;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -25,11 +29,26 @@ public class MainActivity extends AppCompatActivity {
         mEtAddress.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, );
-
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                getSearchResult.launch(intent);
 
             }
         });
-
     }
+
+    private final ActivityResultLauncher<Intent> getSearchResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                // SearchActivity로 부터 결과값이 이곳으로 전달된다.
+                if (result.getResultCode() == RESULT_OK){
+                    if(result.getData() != null) {
+                        String data = result.getData().getStringExtra("data");
+                        mEtAddress.setText(data);
+                    }
+
+                }
+            }
+
+    );
+
 }
--- KakaoAddress/app/src/main/java/com/kakaoaddress/SearchActivity.java
+++ KakaoAddress/app/src/main/java/com/kakaoaddress/SearchActivity.java
@@ -1,8 +1,10 @@
 package com.kakaoaddress;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,16 +22,26 @@ public class SearchActivity extends AppCompatActivity {
         WebView webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.addJavascriptInterface(new BridgeInterface(), "Android");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:sample2_execDaumPostcode");
+            }
+        });
 
+       // webView.loadUrl("호스팅 URL입력");
 
     }
 
     private class BridgeInterface {
         @JavascriptInterface
-        public void processData(String data) {
+        public void processDATA(String data) {
+
+            Intent intent = new Intent();
+            intent.putExtra("data", data);
+            setResult(RESULT_OK,intent);
+            finish();
 
         }
-
-
     }
 }
