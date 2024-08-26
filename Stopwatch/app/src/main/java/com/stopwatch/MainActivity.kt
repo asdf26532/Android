85715f3a9af3a637291486a79bad58068490202a
SeungHwan Lee <asdf26532@gmail.com>
2024-08-26 18:32:35 +0900

    setting

--- Stopwatch/app/src/main/java/com/stopwatch/MainActivity.kt
+++ Stopwatch/app/src/main/java/com/stopwatch/MainActivity.kt
@@ -1,20 +1,63 @@
 package com.stopwatch
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+
+    private lateinit var btn_start : Button
+    private lateinit var btn_refresh : Button
+    private lateinit var tv_minute : TextView
+    private lateinit var tv_second : TextView
+    private lateinit var tv_millisecond : TextView
+
+    private var isRunning = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btn_start = findViewById(R.id.btn_start)
+        btn_refresh = findViewById(R.id.btn_refresh)
+        tv_minute = findViewById(R.id.tv_minute)
+        tv_second = findViewById(R.id.tv_second)
+        tv_millisecond = findViewById(R.id.tv_millisecond)
+
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.btn_start -> {
+                if(isRunning) {
+                    pause()
+                } else {
+                    start()
+                }
+            }
+            R.id.btn_refresh -> {
+                refresh()
+            }
         }
     }
+
+    private fun start(){
+
+    }
+
+    private fun pause(){
+
+    }
+
+    private fun refresh(){
+
+    }
+
+
+
 }
