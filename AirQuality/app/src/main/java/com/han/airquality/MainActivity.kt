a54828da771f65bf7aa0d3e488d0d105e97be37c
SeungHwan Lee <asdf26532@gmail.com>
2024-09-04 19:53:25 +0900

    add permission

--- AirQuality/app/src/main/java/com/han/airquality/MainActivity.kt
+++ AirQuality/app/src/main/java/com/han/airquality/MainActivity.kt
@@ -1,20 +1,125 @@
 package com.han.airquality
 
+import android.Manifest
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.han.airquality.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+
+    val REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    lateinit var getGPSPermissionLauncher : ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        chackAllPermissions()
+
+    }
+
+    private fun chackAllPermissions(){
+        if(!isLocationServicesAvailable()){
+            showDialogForLocationServiceSetting()
+
+        } else {
+            isRunTimePermissionsGranted()
         }
     }
-}
+
+    private fun isLocationServicesAvailable() : Boolean {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+    }
+
+    private fun isRunTimePermissionsGranted() {
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        if(hasFineLocationPermission!=PackageManager.PERMISSION_GRANTED || hasCoarseLocationPermission!=PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this@MainActivity, REQUIRED_PERMISSIONS,PERMISSIONS_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == PERMISSIONS_REQUEST_CODE && grantResults.size == REQUIRED_PERMISSIONS.size) {
+            var checkResult = true
+
+            for(result in grantResults) {
+                if(result != PackageManager.PERMISSION_GRANTED){
+                    checkResult = false
+                    break;
+                }
+            }
+
+            if(checkResult) {
+                // 위치값을 가져올수있음
+            } else {
+                Toast.makeText(this@MainActivity, "권한이 거부되었습니다. 앱을 종료합니다", Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+    }
+
+    private fun showDialogForLocationServiceSetting() {
+        getGPSPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(isLocationServicesAvailable()) {
+                    isRunTimePermissionsGranted()
+                } else {
+                    Toast.makeText(this@MainActivity, "위치 서비스를 사용할 수 없습니다. 앱을 종료합니다", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            }
+
+        }
+
+        val builder : AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage("위치 서비스가 꺼져있습니다. 설정 후 다시 실행해주십시오")
+        builder.setCancelable(true)
+
+        builder.setPositiveButton("설정", DialogInterface.OnClickListener { dialogInterface, i ->
+            val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            getGPSPermissionLauncher.launch(callGPSSettingIntent)
+        })
+
+        builder.setNegativeButton("취소",DialogInterface.OnClickListener { dialogInterface, i ->
+            dialogInterface.cancel()
+            Toast.makeText(this@MainActivity, "위치 서비스를 사용할 수 없습니다. 앱을 종료합니다", Toast.LENGTH_LONG).show()
+            finish()
+        })
+
+        builder.create().show()
+
+        }
+    }
