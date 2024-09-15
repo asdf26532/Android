3003a11879fb757fe514da0eedcad862256ffe9d
SeungHwan Lee <asdf26532@gmail.com>
2024-09-15 23:18:32 +0900

    admob setting

--- AirQuality_ver3/app/build.gradle.kts
+++ AirQuality_ver3/app/build.gradle.kts
@@ -68,6 +68,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
 
+    // admob
+    implementation("com.google.android.gms:play-services-ads:23.3.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
