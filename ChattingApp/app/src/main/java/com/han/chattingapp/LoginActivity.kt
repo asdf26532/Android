218e9b414e88100b15d6f2b6fa8ddd1209b46176
SeungHwan Lee <asdf26532@gmail.com>
2024-09-21 19:14:57 +0900

    signUp

--- ChattingApp/app/src/main/java/com/han/chattingapp/LoginActivity.kt
+++ ChattingApp/app/src/main/java/com/han/chattingapp/LoginActivity.kt
@@ -1,20 +1,33 @@
 package com.han.chattingapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.han.chattingapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 회원가입 버튼 이벤트
+        binding.btnSignUp.setOnClickListener{
+            val intent: Intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+            startActivity(intent)
         }
+
+
+
+
+
     }
 }
