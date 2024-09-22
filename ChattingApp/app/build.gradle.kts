88ee73c8871fb28dbe7f2ca35e419da853a68d5d
SeungHwan Lee <asdf26532@gmail.com>
2024-09-22 21:57:22 +0900

    connect firebase

--- ChattingApp/app/build.gradle.kts
+++ ChattingApp/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,7 +48,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-analytics")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
 }
--- ChattingApp/app/src/main/java/com/han/chattingapp/SignUpActivity.kt
+++ ChattingApp/app/src/main/java/com/han/chattingapp/SignUpActivity.kt
@@ -1,16 +1,57 @@
 package com.han.chattingapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.han.chattingapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
+    lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // 인증 초기화
+        mAuth = Firebase.auth
+
+        binding.btnSignUp.setOnClickListener {
+            val email = binding.edtEmail.text.toString().trim()
+            val password = binding.edtPassword.text.toString().trim()
+
+            signUp(email, password)
+
+        }
+    }
+
+    // 회원 가입 기능
+    private fun signUp(email:String, password:String) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "회원가입 성공",Toast.LENGTH_LONG).show()
+                    val intent: Intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "회원가입 실패",Toast.LENGTH_LONG).show()
+                }
+            }
 
     }
+
+
+
 }
