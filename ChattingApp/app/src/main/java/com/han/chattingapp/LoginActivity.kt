9344ba212ea47ae105f171cc129401144b738dd6
SeungHwan Lee <asdf26532@gmail.com>
2024-09-23 19:07:16 +0900

    Login

--- ChattingApp/app/src/main/java/com/han/chattingapp/LoginActivity.kt
+++ ChattingApp/app/src/main/java/com/han/chattingapp/LoginActivity.kt
@@ -2,16 +2,23 @@ package com.han.chattingapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.han.chattingapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityLoginBinding
+
+    lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +26,39 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 인증 초기화
+        mAuth = Firebase.auth
+
+        // 로그인 버튼 이벤트
+        binding.btnLogin.setOnClickListener {
+
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPassword.text.toString()
+
+            login(email, password)
+        }
+
         // 회원가입 버튼 이벤트
         binding.btnSignUp.setOnClickListener{
             val intent: Intent = Intent(this@LoginActivity, SignUpActivity::class.java)
             startActivity(intent)
         }
+    }
 
-
-
-
-
+    private fun login(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // 성공시 실행
+                    val intent: Intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+                    finish()
+                } else {
+                    // 실패시 실행
+                    Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    Log.d("Login", "Error: ${task.exception}" )
+                }
+            }
     }
 }
