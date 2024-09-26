ac8a59fdf7b3eb6041372a60f6cd340841f385d0
SeungHwan Lee <asdf26532@gmail.com>
2024-09-26 20:38:18 +0900

    userlist

--- ChattingApp/app/src/main/java/com/han/chattingapp/MainActivity.kt
+++ ChattingApp/app/src/main/java/com/han/chattingapp/MainActivity.kt
@@ -5,16 +5,73 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.han.chattingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var adapter: UserAdapter
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
+
+    private lateinit var userList: ArrayList<User>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 인증 초기화
+        mAuth = Firebase.auth
+
+        // DB 초기화
+        mDbRef = Firebase.database.reference
+
+        // 리스트 초기화
+        userList = ArrayList()
+
+        // 어댑터 초기화
+        adapter = UserAdapter(this, userList)
+
+        binding.rvUser.layoutManager = LinearLayoutManager(this)
+        binding.rvUser.adapter = adapter
+
+        // 사용자 정보 가져오기
+        mDbRef.child("user").addValueEventListener(object: ValueEventListener{
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(postSnapshot in snapshot.children){
+                    // 본인제외 나머지 유저 정보
+                    val currentUser = postSnapshot.getValue(User::class.java)
+
+                    if(mAuth.currentUser?.uid != currentUser?.uId){
+                        userList.add(currentUser!!)
+                    }
+                }
+
+                adapter.notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // 실패시 실행
+            }
+
+
+        })
+
+
+
+
     }
 }
