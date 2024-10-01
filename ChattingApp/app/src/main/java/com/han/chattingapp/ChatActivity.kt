0325ffd2d252627844deb2f3ed24285ec83f717e
SeungHwan Lee <asdf26532@gmail.com>
2024-10-01 19:43:24 +0900

    chatting app

--- ChattingApp/app/src/main/java/com/han/chattingapp/ChatActivity.kt
+++ ChattingApp/app/src/main/java/com/han/chattingapp/ChatActivity.kt
@@ -5,9 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.han.chattingapp.databinding.ActivityChatBinding
 import com.han.chattingapp.databinding.ActivityMainBinding
 
@@ -24,11 +28,20 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var receiverRoom: String
     private lateinit var senderRoom: String
 
+    private lateinit var messageList: ArrayList<Message>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        messageList = ArrayList()
+        val messageAdapter: MessageAdapter = MessageAdapter(this, messageList)
+
+        // RecyclerView
+        binding.rvChat.layoutManager = LinearLayoutManager(this)
+        binding.rvChat.adapter = messageAdapter
 
         // 넘어온 데이터 변수에 담기
         receiverName = intent.getStringExtra("name").toString()
@@ -58,10 +71,38 @@ class ChatActivity : AppCompatActivity() {
                     // 저장 성공시
                     mDbRef.child("chats").child(receiverRoom).child("message").push()
                         .setValue(messageObject)
+                }
+            // 입력 부분 초기화
+            binding.edtMessage.setText("")
+        }
 
+        // 메시지 가져오기
+        mDbRef.child("chats").child(senderRoom).child("message")
+            .addValueEventListener(object: ValueEventListener{
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    messageList.clear()
+
+                    for(postSnapshot in snapshot.children) {
+                        val message = postSnapshot.getValue(Message::class.java)
+                        if (message != null) {
+                            messageList.add(message)
+                        }
+                    }
+                    // 적용
+                    messageAdapter.notifyItemInserted(messageList.size - 1)
+                    binding.rvChat.scrollToPosition(messageList.size - 1) // 스크롤을 최신 메시지로 이동
 
                 }
-        }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+
+            })
+
+
 
     }
 }
